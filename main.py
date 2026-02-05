9c80bd748ca6887e162337e50a907b8ca3c1edac
Anagha S Ranjit <anaghasranjit@Anaghas-MacBook-Air.local>
2026-02-05 16:36:42 +0100

    Added PUT & Get  FastAPI endpoints

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+@app.get("/questions")
+async def get_all_questions(db: db_dependency):
+    return db.query(models.Questions).all()
+
 @app.get("/questions/{question_id}")
 async def read_question(question_id: int, db: db_dependency):
     result = db.query(models.Questions).filter(models.Questions.id == question_id).first()
@@ -49,3 +53,17 @@ async def create_questions(question: QuestionBase, db:db_dependency):
         db_choice = models.Choices(choice_text = choice.choice_text, is_correct = choice.is_correct, question_id = db_question.id)
         db.add(db_choice)
     db.commit()
+
+@app.put("/update/questions/{question_id}")
+async def update_question(question_id: int,question: QuestionBase, db: db_dependency):
+    db_question = db.query(models.Questions).filter(models.Questions.id == question_id).first()
+    if not db_question:
+        raise HTTPException(status_code=404, detail='Question not found')
+    db_question.question_text = question.question_text
+    db.query(models.Choices).filter(models.Choices.question_id == question_id).delete()
+    for choice in question.choices:
+        db_choice = models.Choices(choice_text = choice.choice_text, is_correct = choice.is_correct, question_id = db_question.id)
+        db.add(db_choice)
+    db.commit()
+    db.refresh(db_question)
+    return {"message": "Question updated successfully", "question_id": question_id}
